c94e08cc4a0f91d0e4103ac6fad9e1a8bed39bdf
ljk53 <liujiakai@gmail.com>
2021-09-21 08:10:36 -0700

    script to install opencv packages

---
+++ setup-jetson-nano.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt install libopencv-core-dev libopencv-highgui-dev libopencv-calib3d-dev libopencv-objdetect-dev libopencv-photo-dev libopencv-shape-dev libopencv-stitching-dev libopencv-superres-dev libopencv-videostab-dev libopencv-viz-dev
+
